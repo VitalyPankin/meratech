a191eb670f8a083e10ecf68ee13b20b17da27a78
VitalyPankin <pnpsoftworks@gmail.com>
2016-04-11 04:07:34 +0300

    transfer

--- app/router.js
+++ app/router.js
@@ -7,10 +7,31 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('index', { path: '/' });
-  this.route('catalog', { path: '/catalog' });
-  this.route('news', { path: '/news' });
+  this.route('news', function() {
+    this.route('news', {
+      path: '/news/:id'
+    });  
+	});
   this.route('contacts', { path: '/contacts' });
   this.route('industries', { path: '/industries' });
+  // this.route("subscribers", {
+  //   path: '/'
+  // }, function() {
+  //   this.route('subscriber', {
+  //     path: '/subscribers/:id'
+  //   });
+  // });
+  this.route("catalog",  function() {
+    this.route('catalog', {
+      path: '/catalog/:id'
+    });  
+	});
+  this.route('equipment', function() {
+    this.route('equipment', {
+      path: '/equipment/:id'
+    });  
+	});
+
 });
 
 export default Router;
