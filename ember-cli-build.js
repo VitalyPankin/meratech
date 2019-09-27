856e5df96e36b3dc6cf0e170f083e8d41f1c18ec
Ilya Lebedev <ilebedev@freeconferencecall.com>
2019-09-27 12:21:18 +0300

    Ember update. bower is gone

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,54 +3,64 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var pickFiles = require('broccoli-static-compiler'),
-    mergeTrees = require('broccoli-merge-trees'),
-    nodeSass = require('node-sass');
-  
+  mergeTrees = require('broccoli-merge-trees'),
+  nodeSass = require('node-sass');
+
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     sassOptions: {
-      nodeSass: nodeSass
+      implementation: require('node-sass'),
     },
+    // sassOptions: {
+    //   nodeSass: nodeSass
+    // },
     fingerprint: {
-      enabled: false
+      enabled: false,
     },
     sourcemaps: {
-      enabled: true
+      enabled: true,
     },
     minifyJS: {
-      enabled: false
+      enabled: false,
     },
     minifyCSS: {
-      enabled: true
+      enabled: true,
     },
     outputPaths: {
       app: {
         html: 'index.html',
         css: {
-          'app': '/assets/meratech.css'
+          app: '/assets/meratech.css',
         },
-        js: '/assets/meratech.js'
+        js: '/assets/meratech.js',
       },
       vendor: {
         css: '/assets/vendor.css',
-        js: '/assets/vendor.js'
-      }
-    }
+        js: '/assets/vendor.js',
+      },
+    },
   });
 
-  var awesomeFonts = pickFiles('bower_components/font-awesome/fonts', {
-      srcDir: '/',
-      destDir: '/fonts'
-  });
+  // var awesomeFonts = pickFiles('vendor/font-awesome/webfonts', {
+  //   srcDir: '/',
+  //   destDir: '/fonts',
+  // });
+  // var awesomeFonts = pickFiles('bower_components/font-awesome/fonts', {
+  //   srcDir: '/',
+  //   destDir: '/fonts',
+  // });
 
+  app.import('vendor/bootstrap/bootstrap.js');
+  app.import('vendor/jquery/jquery.cookie.js');
+  app.import('vendor/moment/moment.min.js');
 
-  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-  app.import('bower_components/jquery.cookie/jquery.cookie.js');
-  app.import('bower_components/moment/min/moment.min.js');
+  // app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  // app.import('bower_components/jquery.cookie/jquery.cookie.js');
+  // app.import('bower_components/moment/min/moment.min.js');
+
   // app.import('bower_components/jquery-mousewheel/jquery.mousewheel.min.js');
   // app.import('bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css');
   // app.import('bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js');
-
 
   app.import('vendor/revolution/js/jquery.themepunch.revolution.min.js');
   app.import('vendor/revolution/js/jquery.themepunch.tools.min.js');
@@ -58,20 +68,22 @@ module.exports = function(defaults) {
   app.import('vendor/revolution/css/layers.css');
   app.import('vendor/revolution/css/navigation.css');
 
-   var sliderRevolutionExtensions = pickFiles('vendor/revolution/js/extensions', {
-      srcDir: '/',
-      files: ['**/*.min.js'],
-      destDir: '/assets/extensions'
+  var sliderRevolutionExtensions = pickFiles('vendor/revolution/js/extensions', {
+    srcDir: '/',
+    files: ['**/*.min.js'],
+    destDir: '/assets/extensions',
   });
 
+  app.import('vendor/bootstrap/bootstrap.css');
+  // app.import('vendor/font-awesome/css/fontawesome.css');
 
+  // app.import('bower_components/font-awesome/css/font-awesome.css');
+  // app.import('bower_components/font-awesome/css/font-awesome.css.map', { destDir: 'assets' });
+  // app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  // app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', { destDir: 'assets' });
 
-  app.import('bower_components/font-awesome/css/font-awesome.css');
-  app.import('bower_components/font-awesome/css/font-awesome.css.map', { destDir: 'assets' });
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', { destDir: 'assets' });
-
-  var modulesToBuild = [app.toTree(), awesomeFonts, sliderRevolutionExtensions];
+  var modulesToBuild = [app.toTree(), sliderRevolutionExtensions];
+  // var modulesToBuild = [app.toTree(), awesomeFonts, sliderRevolutionExtensions];
 
   return mergeTrees(modulesToBuild);
 };
